c0aac8c0c52c28302d789593202ce84fe8b0d93e
David Karwowski <karwowski.david@gmail.com>
2022-02-18 19:37:45 -0500

    != on one layer

--- layouts/community/split_3x5_3/dkarwowski/keymap.c
+++ layouts/community/split_3x5_3/dkarwowski/keymap.c
@@ -44,15 +44,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [NUM] = LAYOUT_split_3x5_3(
     KC_BSLS, KC_7,    KC_8,    KC_9,    KC_CIRC, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
-    KC_DOT,  KC_4,    KC_5,    KC_6,    KC_PLUS, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+    KC_EXLM, KC_4,    KC_5,    KC_6,    KC_PLUS, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
     KC_0,    KC_1,    KC_2,    KC_3,    KC_ASTR, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-                      KC_DLR,  KC_MINS, KC_EQL,  U_NA,    U_NA,    U_NA
+                      KC_DOT,  KC_MINS, KC_EQL,  U_NA,    U_NA,    U_NA
   ),
   [SYM] = LAYOUT_split_3x5_3(
     KC_GRV,  KC_TILD, KC_LPRN, KC_RPRN, KC_HASH, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
     KC_ASTR, KC_AMPR, KC_LCBR, KC_RCBR, KC_PERC, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_SCLN, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-                      KC_PIPE, KC_COLN, KC_UNDS, U_NA,    U_NA,    U_NA
+    KC_PIPE, KC_AT,   KC_LBRC, KC_RBRC, KC_SCLN, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
+                      KC_DLR,  KC_COLN, KC_UNDS, U_NA,    U_NA,    U_NA
   ),
   [FUN] = LAYOUT_split_3x5_3(
     KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
